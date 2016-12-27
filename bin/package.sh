3c8c178809116bdefec411fa59a5f901f8ba431c
Josa Gesell <josa@gesell.me>
2016-12-27 11:33:08 +0100

    📦  Add update icons script

--- bin/package.sh
+++ bin/package.sh
@@ -3,8 +3,4 @@
 mkdir -p dist
 rm -f dist/move-tab.zip
 
-convert -resize 128x128 docs/icon/move-tab-icon.png icon-128.png
-convert -resize 48x48 docs/icon/move-tab-icon.png icon-48.png
-convert -resize 16x16 docs/icon/move-tab-icon.png icon-16.png
-
 zip -r dist/move-tab.zip . -x .git\* docs\* bin\* dist\* .\*
---
+++ bin/update-icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+convert -resize 128x128 docs/icon/move-tab-icon.png icon-128.png
+convert -resize 48x48 docs/icon/move-tab-icon.png icon-48.png
+convert -resize 16x16 docs/icon/move-tab-icon.png icon-16.png
